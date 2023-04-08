efc39063652c9db6eab031f0ccf3a697d34bab10
lundih <lundih27@gmail.com>
2023-04-08 13:06:38 +0300

    Add test contoller

---
+++ src/main/java/com/lundih/usercontact/rest/TestResource.java
@@ -0,0 +1,28 @@
+package com.lundih.usercontact.rest;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Rest controller to handle requests to check if the server is up
+ *
+ * @author lundih
+ * @since 0.0.1
+ *
+ */
+@Tag(name = "Test resource", description = "Test Endpoints")
+@RestController
+@RequestMapping("api/v1/test")
+public class TestResource {
+    /**
+     * Endpoint that returns a message. This can be used for smoke test purposes
+     *
+     * @return String that indicates the server is running
+     */
+    @GetMapping
+    String testApp() {
+        return "The server is up!";
+    }
+}
